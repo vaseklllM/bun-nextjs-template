6d84788a611c723856a9bea91547d88fac6d2703
vasek <vasia.motnin@gmail.com>
2025-05-09 13:56:59 +0200

    update structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,72 @@
-import Image from "next/image";
+"use client";
+import { FormEventHandler, useCallback, useState } from "react";
+import { ATMService, Nominal } from "@/entities/atm";
+import { Button } from "@/shared/ui/Button";
+import { Input } from "@/shared/ui/Input";
+import { Pre } from "@/shared/ui/Pre";
+import { Auth } from "@/features/auth";
+import { Blog } from "@/pages/blog";
+import { NewsBlog } from "@/widgets/news-block";
 
 export default function Home() {
-  return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [nominals, setNominals] = useState<Nominal[]>([]);
+  const [isHaveAmount, setIsHaveAmount] = useState<boolean>(true);
 
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const submitHandler = useCallback<FormEventHandler<HTMLFormElement>>(
+    (event) => {
+      event.preventDefault();
+      const form = new FormData(event.currentTarget);
+      const amountFormValue = form.get("amount");
+
+      if (amountFormValue !== null) {
+        const amount = Number(amountFormValue);
+        if (!isNaN(amount)) {
+          const atm = new ATMService();
+          const result = atm.getNominals(amount);
+          if (result.isHaveFullAmount) {
+            setNominals(result.nominals);
+          } else {
+            setNominals([]);
+          }
+          setIsHaveAmount(result.isHaveFullAmount);
+        }
+      }
+    },
+    [setNominals],
+  );
+
+  return (
+    <div>
+      <Auth />
+      <Blog />
+      <NewsBlog />
+      <h2>ATM 2</h2>
+      <br />
+      <form onSubmit={submitHandler} className="w-100 pl-5">
+        <label className="block text-sm font-medium text-gray-900 dark:text-white">
+          Amount
+        </label>
+        <Input
+          type="number"
+          name="amount"
+          placeholder="0"
+          required
+          className="mt-1"
+        />
+        <Button className="mt-6" type="submit">
+          Submit
+        </Button>
+      </form>
+      <br />
+      {!isHaveAmount && <p>The ATM doesn`t have current amount.</p>}
+      <br />
+      <div className="flex gap-10">
+        <Pre title="Nominals" data={nominals} />
+        <Pre
+          title="nominal_x_number"
+          data={ATMService.convertNominalToArray(nominals)}
+        />
+      </div>
     </div>
   );
 }
